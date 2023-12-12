acc068672e3bcc540d85d57b9e1243e2568ed934
Abhishek Verma <ak8888780@gmail.com>
2023-12-12 21:08:17 +0530

    main URL shortner file

--- api/main.go
+++ api/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os" // to access env variable
+	"url-shortner/api/routes"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/middleware/logger"
-	"github.com/joho/godotenv"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func setupRoutes(app *fiber.App) {
@@ -16,17 +15,10 @@ func setupRoutes(app *fiber.App) {
 }
 
 func main() {
-	err := godotenv.Load()
-
-	if err != nil {
-		fmt.Println(err)
-	}
 	app := fiber.New()
-
 	app.Use(logger.New())
 
 	setupRoutes(app)
-
 	log.Fatal(app.Listen(os.Getenv("APP_PORT"))) //Create the sever here
 
 }
