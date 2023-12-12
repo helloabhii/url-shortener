2c7f1c80d8d940e6ecc4ad47e4d161afcbb4c4c3
Abhishek Verma <ak8888780@gmail.com>
2023-12-12 21:07:55 +0530

    Database file

--- api/database/database.go
+++ api/database/database.go
@@ -10,11 +10,12 @@ import (
 var Ctx = context.Background()
 
 func CreateClient(dbNo int) *redis.Client {
+
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     os.Getenv("DB_ADDR"),
 		Password: os.Getenv("DB_PASS"),
 		DB:       dbNo,
 	})
-	return rdb
+	return rdb, nil
 
 }
