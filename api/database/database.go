1778b1781480c03ce0ff0d5dee14cef7431b738b
Abhishek Verma <ak8888780@gmail.com>
2023-12-21 08:00:49 +0530

    database file'

--- api/database/database.go
+++ api/database/database.go
@@ -4,16 +4,16 @@ import (
 	"context"
 	"os"
 
-	"github.com/go-redis/redis/v8"
+	"github.com/go-redis/redis/v8" //here we use this database  //v8 stable version
 )
 
 var Ctx = context.Background()
 
-func CreateClient(dbNo int) *redis.Client {
+func CreateClient(dbNo int) *redis.Client { //this func returns -> redis.Client
 
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("DB_ADDR"),
-		Password: os.Getenv("DB_PASS"),
+		Addr:     os.Getenv("DB_ADDR"), //created in env file
+		Password: os.Getenv("DB_PASS"), //getting this from our file
 		DB:       dbNo,
 	})
 	return rdb
